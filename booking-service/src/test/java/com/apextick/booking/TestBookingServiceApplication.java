058084f6a9c9fb02498e6aea9e385e036a6d5677
kalanas210 <kalanasandakelum210@gmail.com>
2026-06-05 18:32:22 +0530

    project setup

---
+++ booking-service/src/test/java/com/apextick/booking/TestBookingServiceApplication.java
@@ -0,0 +1,11 @@
+package com.apextick.booking;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(BookingServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
